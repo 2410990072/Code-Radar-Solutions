451c9f4775e0591132d9ed594cc044bdb03355ec
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-02-07 22:07:31 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,operation;
+    scanf("%d %d",&a,&b);
+    if(operator==+)
+    printf(a+b);
+    else if(operator==-)
+    printf(a-b);
+    else if(operator==*)
+    printf(a*b);
+    else if(operator==/)
+    printf(a/b);
+
+    return 0;
+}
