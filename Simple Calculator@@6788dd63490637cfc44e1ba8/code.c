e62736d29241bfed42d461fdcf47b8f28038b84a
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-02-12 20:28:28 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    float a,b;
-    char oper;
-    scanf("%c",&oper);
-    scanf("%lf %lf",&a,&b);
-    if(oper=='+')
-    printf("%lf + %lf=%.2lf\n",a,b,a+b);
-    else if(oper=='-')
-    printf("%lf - %lf=%.2lf\n",a,b,a-b);
-    else if(oper=='*')
-    printf("%lf * %lf=%.2lf\n",a,b,a*b);
-    else if(oper=='/')
-    printf("%lf / %lf=%.2lf\n",a,b,a/b);
-    else
-    printf("error");
+    double num1, num2, result;
+    char operator;
+
+    // Get user input
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // The space before %c is used to skip any whitespace character
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    // Perform the operation based on the operator
+    if (operator == '+') {
+        result = num1 + num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '-') {
+        result = num1 - num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '*') {
+        result = num1 * num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '/') {
+        if (num2 != 0) {  // Check to prevent division by zero
+            result = num1 / num2;
+            printf("Result: %.2lf\n", result);
+        } else {
+            printf("error\n");
+        }
+    } 
+   
 
     return 0;
 }
