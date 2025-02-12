5e5b4cc53eaaf2d8285894eed94f98bf0c7ae6db
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-02-12 20:29:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,34 +3,29 @@
 int main() {
     double num1, num2, result;
     char operator;
-
-    // Get user input
-    printf("Enter first number: ");
     scanf("%lf", &num1);
 
-    printf("Enter an operator (+, -, *, /): ");
-    scanf(" %c", &operator);  // The space before %c is used to skip any whitespace character
-
-    printf("Enter second number: ");
+    printf("(+, -, *, /): ");
+    scanf(" %c", &operator); 
     scanf("%lf", &num2);
 
-    // Perform the operation based on the operator
+   
     if (operator == '+') {
         result = num1 + num2;
-        printf("Result: %.2lf\n", result);
+        printf("%.2lf\n", result);
     }
     else if (operator == '-') {
         result = num1 - num2;
-        printf("Result: %.2lf\n", result);
+        printf("%.2lf\n", result);
     }
     else if (operator == '*') {
         result = num1 * num2;
-        printf("Result: %.2lf\n", result);
+        printf("%.2lf\n", result);
     }
     else if (operator == '/') {
         if (num2 != 0) {  // Check to prevent division by zero
             result = num1 / num2;
-            printf("Result: %.2lf\n", result);
+            printf("%.2lf\n", result);
         } else {
             printf("error\n");
         }
