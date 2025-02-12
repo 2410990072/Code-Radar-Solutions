f9d787be3d1b7e16f0d30e613ba05bfd2975c1ec
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-02-12 20:17:08 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,13 +6,13 @@ int main() {
     scanf("%c",&oper);
     scanf("%f %f",&a,&b);
     if(oper=='+')
-    printf("%.2f+%.2f=%.2f\n",a,b,a+b);
+    printf("%.2f + %.2f=%.2f\n",a,b,a+b);
     else if(oper=='-')
-    printf("%.2f-%.2f=%.2f\n",a,b,a-b);
+    printf("%.2f - %.2f=%.2f\n",a,b,a-b);
     else if(oper=='*')
-    printf("%.2f*%.2f=%.2f\n",a,b,a*b);
+    printf("%.2f * %.2f=%.2f\n",a,b,a*b);
     else if(oper=='/')
-    printf("%.2f/%.2f=%.2f\n",a,b,a/b);
+    printf("%.2f / %.2f=%.2f\n",a,b,a/b);
     else
     printf("error");
 
