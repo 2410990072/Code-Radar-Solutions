413d5f423b4af4f2a4090457d66cb5499014f84e
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-03-30 10:09:09 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,33 +1,28 @@
 #include<stdio.h>
-
-void fibonacciseries(int n);
-
-int main() {
+void fibonacciSeries(int n);
+int main(){
     int n;
-    scanf("%d", &n); // Get the input
-    if (n <= 0) {
+    scanf("%d",&n);
+    if(n<=0){
         printf("Invalid input\n");
-        return 0; // Exit if the input is invalid
+        return 0;
     }
-    fibonacciseries(n); // Print the Fibonacci series
+    fibonacciSeries(n);
     return 0;
 }
-
-void fibonacciseries(int n) {
-    // Variables to store the previous two Fibonacci numbers
-    int a = 0, b = 1, next;
-
+void fibonacciSeries(int n){
+    int a=0,b=1,next;
     // Special case for n = 1
     if (n >= 1) printf("%d\n", a);
 
     // Special case for n = 2
     if (n >= 2) printf("%d\n", b);
 
-    // Generate the remaining Fibonacci numbers
+    // Generate and print Fibonacci numbers starting from the 3rd number
     for (int i = 3; i <= n; i++) {
         next = a + b;
         printf("%d\n", next);
-        a = b; // Update a to be the previous number
-        b = next; // Update b to be the current number
-    }
+        a = b; // Update a to the previous Fibonacci number
+        b = next;
+}
 }
