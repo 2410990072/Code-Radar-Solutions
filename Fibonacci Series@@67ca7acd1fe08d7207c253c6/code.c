ebd5af4e9d17ac711142fa0d186add1216c60030
Shruti Gupta <shruti0072.becse24@chitkara.edu.in>
2025-03-30 10:04:22 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,21 +1,33 @@
 #include<stdio.h>
-void fibonacciseries(int n);
-int main(){
-    int n;
-    scanf("%d\n",&n);
-    if(n<=0){
-        printf("Invalid input\n");
-    }
-    fibonacciseries(3);
-    for(int i=2;i<n;i++){
-        fib[i]=fib[i-1]+fib[i-2];
-        printf("%d\n",fib[i]);
-    }
-    printf("\n");
 
+void fibonacciseries(int n);
+
+int main() {
+    int n;
+    scanf("%d", &n); // Get the input
+    if (n <= 0) {
+        printf("Invalid input\n");
+        return 0; // Exit if the input is invalid
+    }
+    fibonacciseries(n); // Print the Fibonacci series
     return 0;
 }
-void fibonacciseries(int n){
-    printf("%d",fibonacciseries);
-}
 
+void fibonacciseries(int n) {
+    // Declare an array to store Fibonacci numbers
+    int fib[n]; 
+
+    // Initializing the first two numbers of the Fibonacci sequence
+    fib[0] = 0;
+    fib[1] = 1;
+
+    // Print the first two Fibonacci numbers
+    if (n >= 1) printf("%d\n", fib[0]);
+    if (n >= 2) printf("%d\n", fib[1]);
+
+    // Generate and print the remaining Fibonacci numbers
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+        printf("%d\n", fib[i]);
+    }
+}
